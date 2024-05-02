9b3551b54d8aee3b33b8bf03d6ec9dd8d10f3670
angeloken <angeloken17@gmail.com>
2024-05-03 00:42:44 +0700

    karsajobs

--- build_push_image_karsajobs.sh
+++
@@ -1,4 +0,0 @@
-echo %CR_PAT% | docker login ghcr.io --username angeloken --password-stdin
-docker build -t karsajobs .
-docker tag karsajobs ghcr.io/angeloken/karsajobs:latest
-docker push ghcr.io/angeloken/karsajobs:latest
